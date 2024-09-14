fd77972f2a91981b03234b3756b827e861e0ae55
anicraj456 <anicraj123@gmail.com>
2024-09-14 08:08:18 -0500

    initial spring project files
    initial project

---
+++ src/main/java/com/anitha/anirecipe/AnirecipeApplication.java
@@ -0,0 +1,13 @@
+package com.anitha.anirecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnirecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnirecipeApplication.class, args);
+	}
+
+}
